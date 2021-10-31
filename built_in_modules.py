3b2ab821da9d92fdae2bfe8f05c1741bc1a9bb27
KseniaMusagitova <kseniamusagitova@iMac-IURII.attlocal.net>
2021-10-30 20:43:46 -0500

    examples of using modules

---
+++ built_in_modules.py
@@ -0,0 +1,23 @@
+# 1
+# import random
+# x = random.randint(1, 10)
+# print(x)
+
+# 2
+from random import randint
+x = randint(1, 10)
+print(x)
+
+# 3
+from random import shuffle
+my_list = [1, 2, 3]
+shuffle(my_list)
+print(my_list)
+
+# 4
+from random import shuffle as shuffle_my_list
+my_list = [1, 2, 3]
+shuffle_my_list(my_list)
+print(my_list)
+
+
